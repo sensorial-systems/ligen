3f39563dbf45f2d0b4fc46551a457bb2facd8532
Danilo Guanabara <danguafer@gmail.com>
2019-10-05 08:13:56 -0300

    Update for presentation

--- src/lib.rs
+++ src/lib.rs
@@ -5,28 +5,35 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input};
 
-use ligen_core::{Object};
-use ligen_core::{Generator};
+use ligen_core::{Object, Generator, Attribute};
 
 #[proc_macro_attribute]
 pub fn ligen(args: TokenStream, input: TokenStream) -> TokenStream {
     let args = parse_macro_input!(args as syn::AttributeArgs);
-    let input = parse_macro_input!(input as syn::ItemImpl); // same as let input : syn::ItemImpl = syn::parse(input).unwrap();
-    let object = Object::parse(input.clone());
+    let attribute = Attribute::parse_args(&args);
 
-    for arg in args {
-        let generator = Generator::new(&arg);
-        match generator {
-            Ok(generator) => {
-                generator.generate(&object);
-            },
-            Err(err) => println!("{}", err),
+    let parsed : syn::Result<syn::ItemImpl> = syn::parse(input.clone());
+
+    let output = match &parsed {
+        Result::Ok(input) => {
+            let object = Object::parse(input.clone());
+
+            let generator = Generator::new(&attribute);
+            match generator {
+                Ok(generator) => generator.generate(&object),
+                Err(err) => eprintln!("{}", err)
+            }
+
+            let output = TokenStream::from(quote!{
+                #input
+                #object
+            });
+            output
+        },
+        Result::Err(_) => {
+            input
         }
-    }
+    };
 
-    let output = TokenStream::from(quote!{
-        #input
-        #object
-    });
     output
 }
