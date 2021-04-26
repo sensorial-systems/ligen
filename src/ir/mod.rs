51c92bad7615496708bff51f8d7fe752c2f5e7e1
gabriel.a <arrudagates@gmail.com>
2021-04-26 16:37:17 -0300

    separated atomic from types and finished types

--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -1,11 +1,13 @@
 //! Ligen intermediate representation.
 
+mod atomic;
 mod attribute;
 mod identifier;
 mod literal;
 mod parser;
 mod types;
 
+pub use atomic::*;
 pub use attribute::*;
 pub use identifier::*;
 pub use literal::*;
--- src/ir/types.rs
+++ src/ir/types.rs
@@ -1,132 +1,77 @@
-use proc_macro2::Ident;
-use syn::{Path, TypePath, TypeReference};
+use syn::{TypePath, TypePtr, TypeReference};
 
+use crate::ir::Atomic;
 use crate::ir::Identifier;
-
-#[derive(Debug, Copy, Clone, PartialEq)]
-/// Integer Enum
-pub enum Integer {
-    /// u8 variant
-    U8,
-    /// u16 variant
-    U16,
-    /// u32 variant
-    U32,
-    /// u64 variant
-    U64,
-    /// u128 variant
-    U128,
-    /// usize variant
-    USize,
-    /// i8 variant
-    I8,
-    /// i16 variant
-    I16,
-    /// i32 variant
-    I32,
-    /// i64 variant
-    I64,
-    /// i128 variant
-    I128,
-    /// isize variant
-    ISize,
-}
-
-#[derive(Debug, PartialEq, Copy, Clone)]
-/// Float Enum
-pub enum Float {
-    /// f32 variant
-    F32,
-    /// f64 variant
-    F64,
-}
-
-#[derive(Debug, PartialEq, Clone, Copy)]
-/// Atomic Enum
-pub enum Atomic {
-    /// Integer variant
-    Integer(Integer),
-    /// Float variant
-    Float(Float),
-    /// Boolean variant
-    Boolean,
-    /// Character variant
-    Character,
-}
-
-#[derive(Debug, PartialEq)]
-// TODO: Find better name for enum
-/// Atom Enum
-pub enum Atom {
-    /// Atomic variant
-    Atomic(Atomic),
-    /// Compound variant
-    Compound(Identifier),
-}
-
-#[derive(Debug, PartialEq)]
-/// Borrowed Enum
-pub enum Borrowed {
-    /// Shared variant
-    Shared(Atom),
-    /// Exclusive variant
-    Exclusive(Atom),
-}
 
 #[derive(Debug, PartialEq)]
 /// Type Enum
 pub enum Type {
-    /// Owned variant
-    Owned(Atom),
-    /// Borrowed variant
-    Borrowed(Borrowed),
+    /// Atomic variant
+    Atomic(Atomic),
+    /// Compound variant
+    Compound(Identifier),
+    /// Reference variant
+    Reference(Reference),
 }
 
-impl From<Ident> for Atom {
-    fn from(ident: Ident) -> Self {
-        println!("ident: {:#?}", ident);
-        match ident.to_string().as_str() {
-            "u8" => Self::Atomic(Atomic::Integer(Integer::U8)),
-            "u16" => Self::Atomic(Atomic::Integer(Integer::U16)),
-            "u32" => Self::Atomic(Atomic::Integer(Integer::U32)),
-            "u64" => Self::Atomic(Atomic::Integer(Integer::U64)),
-            "u128" => Self::Atomic(Atomic::Integer(Integer::U128)),
-            "usize" => Self::Atomic(Atomic::Integer(Integer::USize)),
-            "i8" => Self::Atomic(Atomic::Integer(Integer::I8)),
-            "i16" => Self::Atomic(Atomic::Integer(Integer::I16)),
-            "i32" => Self::Atomic(Atomic::Integer(Integer::I32)),
-            "i64" => Self::Atomic(Atomic::Integer(Integer::I64)),
-            "i128" => Self::Atomic(Atomic::Integer(Integer::I128)),
-            "isize" => Self::Atomic(Atomic::Integer(Integer::ISize)),
-            "f32" => Self::Atomic(Atomic::Float(Float::F32)),
-            "f64" => Self::Atomic(Atomic::Float(Float::F64)),
-            "bool" => Self::Atomic(Atomic::Boolean),
-            "char" => Self::Atomic(Atomic::Character),
-            _ => panic!("Unknown Ident"),
-        }
-    }
+#[derive(Debug, PartialEq)]
+/// Reference Enum
+pub enum Reference {
+    /// Borrow variant
+    Borrow(Borrow),
+    /// Pointer variant
+    Pointer(Pointer),
 }
 
-impl From<Path> for Atom {
-    fn from(path: Path) -> Self {
-        match path {
-            Path { segments, .. } => Self::from(segments[0].ident.clone()),
-        }
-    }
+#[derive(Debug, PartialEq)]
+/// Borrow Enum
+pub enum Borrow {
+    /// Constant variant
+    Constant(Box<Type>),
+    /// Mutable variant
+    Mutable(Box<Type>),
+}
+
+#[derive(Debug, PartialEq)]
+/// Pointer Enum
+pub enum Pointer {
+    /// Constant variant
+    Constant(Box<Type>),
+    /// Mutable variant
+    Mutable(Box<Type>),
 }
 
 impl From<syn::Type> for Type {
     fn from(syn_type: syn::Type) -> Self {
-        println!("syn_type: {:#?}", syn_type);
         match syn_type {
-            syn::Type::Path(TypePath { path, .. }) => Self::Owned(Atom::from(path)),
+            syn::Type::Path(TypePath { path, .. }) => Self::Atomic(Atomic::from(path)),
             syn::Type::Reference(TypeReference {
                 elem, mutability, ..
             }) => {
                 if let syn::Type::Path(TypePath { path, .. }) = *elem {
                     match mutability {
-                        Some(_m) => Self::Borrowed(Borrowed::Exclusive(Atom::from(path))),
-                        None => Self::Borrowed(Borrowed::Shared(Atom::from(path))),
+                        Some(_m) => Self::Reference(Reference::Borrow(Borrow::Mutable(Box::new(
+                            Type::Atomic(Atomic::from(path)),
+                        )))),
+                        None => Self::Reference(Reference::Borrow(Borrow::Constant(Box::new(
+                            Type::Atomic(Atomic::from(path)),
+                        )))),
+                    }
+                } else {
+                    panic!("Unknown type");
+                }
+            }
+            syn::Type::Ptr(TypePtr {
+                elem, mutability, ..
+            }) => {
+                if let syn::Type::Path(TypePath { path, .. }) = *elem {
+                    match mutability {
+                        Some(_m) => Self::Reference(Reference::Pointer(Pointer::Mutable(
+                            Box::new(Type::Atomic(Atomic::from(path))),
+                        ))),
+                        None => Self::Reference(Reference::Pointer(Pointer::Constant(Box::new(
+                            Type::Atomic(Atomic::from(path)),
+                        )))),
                     }
                 } else {
                     panic!("Unknown type");
@@ -141,14 +86,13 @@ impl From<syn::Type> for Type {
 #[cfg(test)]
 mod test {
 
-    use super::{Atom, Atomic, Borrowed, Float, Integer, Type};
+    use super::{
+        Atomic::{self, Boolean, Character},
+        Borrow, Pointer, Reference, Type,
+    };
+    use crate::ir::{Float, Integer};
     use quote::quote;
     use syn::parse_quote::parse;
-
-    #[test]
-    fn types_array() {
-        let a: Type = parse::<syn::Type>(quote! {Vec<i32>}).into();
-    }
 
     #[test]
     fn types_integer() {
@@ -184,7 +128,7 @@ mod test {
             Integer::ISize,
         ]
         .into_iter()
-        .map(|x| Type::Owned(Atom::Atomic(Atomic::Integer(x))))
+        .map(|x| Type::Atomic(Atomic::Integer(x)))
         .collect();
 
         let mut iter = vec.iter().zip(expected.iter());
@@ -202,7 +146,7 @@ mod test {
             .collect();
         let expected: Vec<Type> = vec![Float::F32, Float::F64]
             .into_iter()
-            .map(|x| Type::Owned(Atom::Atomic(Atomic::Float(x))))
+            .map(|x| Type::Atomic(Atomic::Float(x)))
             .collect();
 
         let mut iter = vec.iter().zip(expected.iter());
@@ -215,7 +159,7 @@ mod test {
     #[test]
     fn types_boolean() {
         assert_eq!(
-            Type::Owned(Atom::Atomic(Atomic::Boolean)),
+            Type::Atomic(Boolean),
             parse::<syn::Type>(quote! {bool}).into()
         );
     }
@@ -223,28 +167,48 @@ mod test {
     #[test]
     fn types_character() {
         assert_eq!(
-            Type::Owned(Atom::Atomic(Atomic::Character)),
+            Type::Atomic(Character),
             parse::<syn::Type>(quote! {char}).into()
         );
     }
 
     #[test]
-    fn types_borrowed_shared() {
+    fn types_borrow_constant() {
         assert_eq!(
-            Type::Borrowed(Borrowed::Shared(Atom::Atomic(Atomic::Integer(
-                Integer::I32
-            )))),
+            Type::Reference(Reference::Borrow(Borrow::Constant(Box::new(Type::Atomic(
+                Atomic::Integer(crate::ir::Integer::I32)
+            ))))),
             parse::<syn::Type>(quote! {&i32}).into()
         );
     }
 
     #[test]
-    fn types_borrowed_exclusive() {
+    fn types_borrow_mutable() {
         assert_eq!(
-            Type::Borrowed(Borrowed::Exclusive(Atom::Atomic(Atomic::Integer(
-                Integer::I32
-            )))),
+            Type::Reference(Reference::Borrow(Borrow::Mutable(Box::new(Type::Atomic(
+                Atomic::Integer(crate::ir::Integer::I32)
+            ))))),
             parse::<syn::Type>(quote! {&mut i32}).into()
+        );
+    }
+
+    #[test]
+    fn types_pointer_constant() {
+        assert_eq!(
+            Type::Reference(Reference::Pointer(Pointer::Constant(Box::new(
+                Type::Atomic(Atomic::Integer(crate::ir::Integer::I32))
+            )))),
+            parse::<syn::Type>(quote! {*const i32}).into()
+        );
+    }
+
+    #[test]
+    fn types_pointer_mutable() {
+        assert_eq!(
+            Type::Reference(Reference::Pointer(Pointer::Mutable(Box::new(
+                Type::Atomic(Atomic::Integer(crate::ir::Integer::I32))
+            )))),
+            parse::<syn::Type>(quote! {*mut i32}).into()
         );
     }
 }
