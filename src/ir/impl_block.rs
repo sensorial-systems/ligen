24533e44c1b5167a991fced31cf59639149f1d19
gabriel.a <arrudagates@gmail.com>
2021-04-28 14:35:15 -0300

    implemented ImplItemConst

--- src/ir/impl_block.rs
+++ src/ir/impl_block.rs
@@ -6,8 +6,6 @@ use syn::ItemImpl;
 pub struct Impl {
     /// attributes field
     pub attributes: Attributes,
-    /// trait_ field
-    pub trait_: Option<Identifier>,
     /// self_ field
     pub self_: Identifier,
     /// items field
@@ -18,14 +16,6 @@ impl From<ItemImpl> for Impl {
     fn from(item_impl: ItemImpl) -> Self {
         println!("{:#?}", item_impl);
 
-        let trait_ = match item_impl.trait_ {
-            None => None,
-            Some((Some(_), path, _)) => Some(Identifier {
-                name: String::from(format!("!{}", path.segments[0].ident.clone())),
-            }),
-            Some((None, path, _)) => Some(Identifier::from(path.segments[0].ident.clone())),
-        };
-
         if let syn::Type::Path(syn::TypePath { path, .. }) = *item_impl.self_ty {
             Self {
                 attributes: Attributes {
@@ -35,7 +25,6 @@ impl From<ItemImpl> for Impl {
                         .map(|x| Attribute::from(x.parse_meta().expect("Failed to parse Meta")))
                         .collect(),
                 },
-                trait_,
                 self_: Identifier::from(path.segments[0].ident.clone()),
                 items: None,
             }
@@ -58,41 +47,6 @@ mod test {
             Impl::from(parse::<ItemImpl>(quote! {impl Test {}})),
             Impl {
                 attributes: Attributes { attributes: vec![] },
-                trait_: None,
-                self_: Identifier {
-                    name: String::from("Test")
-                },
-                items: None
-            }
-        );
-    }
-
-    #[test]
-    fn impl_block_trait() {
-        assert_eq!(
-            Impl::from(parse::<ItemImpl>(quote! {impl Trait for Test {}})),
-            Impl {
-                attributes: Attributes { attributes: vec![] },
-                trait_: Some(Identifier {
-                    name: String::from("Trait")
-                }),
-                self_: Identifier {
-                    name: String::from("Test")
-                },
-                items: None
-            }
-        );
-    }
-
-    #[test]
-    fn impl_block_trait_bang() {
-        assert_eq!(
-            Impl::from(parse::<ItemImpl>(quote! {impl !Trait for Test {}})),
-            Impl {
-                attributes: Attributes { attributes: vec![] },
-                trait_: Some(Identifier {
-                    name: String::from("!Trait")
-                }),
                 self_: Identifier {
                     name: String::from("Test")
                 },
@@ -120,7 +74,6 @@ mod test {
                         }
                     )]
                 },
-                trait_: None,
                 self_: Identifier {
                     name: String::from("Test")
                 },
