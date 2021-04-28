c2ca22e32c981d46f2c6b07ffc35cba9c06f3bdb
gabriel.a <arrudagates@gmail.com>
2021-04-28 15:51:41 -0300

    Complete implementation of Impl Block

--- src/ir/impl_block.rs
+++ src/ir/impl_block.rs
@@ -1,4 +1,4 @@
-use crate::ir::{Attribute, Attributes, Identifier};
+use crate::ir::{Attribute, Attributes, Constant, Function, Identifier};
 use syn::ItemImpl;
 
 #[derive(Debug, PartialEq)]
@@ -9,13 +9,34 @@ pub struct Impl {
     /// self_ field
     pub self_: Identifier,
     /// items field
-    pub items: Option<()>,
+    pub items: Vec<ImplItem>,
+}
+
+#[derive(Debug, PartialEq)]
+/// ImplItem Enum
+pub enum ImplItem {
+    /// Constant variant
+    Constant(Constant),
+    /// Method variant
+    Method(Function),
+}
+
+impl From<syn::ImplItem> for ImplItem {
+    fn from(impl_item: syn::ImplItem) -> Self {
+        match impl_item {
+            syn::ImplItem::Const(impl_item_const) => {
+                Self::Constant(Constant::from(impl_item_const))
+            }
+            syn::ImplItem::Method(impl_item_method) => {
+                Self::Method(Function::from(impl_item_method))
+            }
+            _ => panic!("Only Const and Method Impl items are currently supported"),
+        }
+    }
 }
 
 impl From<ItemImpl> for Impl {
     fn from(item_impl: ItemImpl) -> Self {
-        println!("{:#?}", item_impl);
-
         if let syn::Type::Path(syn::TypePath { path, .. }) = *item_impl.self_ty {
             Self {
                 attributes: Attributes {
@@ -26,7 +47,11 @@ impl From<ItemImpl> for Impl {
                         .collect(),
                 },
                 self_: Identifier::from(path.segments[0].ident.clone()),
-                items: None,
+                items: item_impl
+                    .items
+                    .into_iter()
+                    .map(|x| ImplItem::from(x))
+                    .collect(),
             }
         } else {
             panic!("Impl Block Identifier not found")
@@ -36,8 +61,8 @@ impl From<ItemImpl> for Impl {
 
 #[cfg(test)]
 mod test {
-    use super::{Attribute, Attributes, Identifier, Impl, ItemImpl};
-    use crate::ir::Literal;
+    use super::{Attribute, Attributes, Constant, Function, Identifier, Impl, ImplItem, ItemImpl};
+    use crate::ir::{Atomic, Integer, Literal, Type};
     use quote::quote;
     use syn::parse_quote::parse;
 
@@ -50,7 +75,7 @@ mod test {
                 self_: Identifier {
                     name: String::from("Test")
                 },
-                items: None
+                items: vec![]
             }
         );
     }
@@ -77,7 +102,93 @@ mod test {
                 self_: Identifier {
                     name: String::from("Test")
                 },
-                items: None
+                items: vec![]
+            }
+        );
+    }
+
+    #[test]
+    fn impl_block_items_const() {
+        assert_eq!(
+            Impl::from(parse::<ItemImpl>(quote! {
+                impl Test {
+                    const a: i32 = 2;
+                }
+            })),
+            Impl {
+                attributes: Attributes { attributes: vec![] },
+                self_: Identifier {
+                    name: String::from("Test")
+                },
+                items: vec![ImplItem::Constant(Constant {
+                    identifier: Identifier {
+                        name: String::from("a")
+                    },
+                    type_: Type::Atomic(Atomic::Integer(Integer::I32)),
+                    literal: Literal::Integer(2)
+                })]
+            }
+        );
+    }
+
+    #[test]
+    fn impl_block_items_method() {
+        assert_eq!(
+            Impl::from(parse::<ItemImpl>(quote! {
+                impl Test {
+                    fn a(){}
+                }
+            })),
+            Impl {
+                attributes: Attributes { attributes: vec![] },
+                self_: Identifier {
+                    name: String::from("Test")
+                },
+                items: vec![ImplItem::Method(Function {
+                    attributes: Attributes { attributes: vec![] },
+                    asyncness: None,
+                    identifier: Identifier {
+                        name: String::from("a")
+                    },
+                    input: vec![],
+                    output: None
+                })]
+            }
+        );
+    }
+
+    #[test]
+    fn impl_block_items() {
+        assert_eq!(
+            Impl::from(parse::<ItemImpl>(quote! {
+                impl Test {
+                    const a: i32 = 2;
+                    fn b(){}
+                }
+            })),
+            Impl {
+                attributes: Attributes { attributes: vec![] },
+                self_: Identifier {
+                    name: String::from("Test")
+                },
+                items: vec![
+                    ImplItem::Constant(Constant {
+                        identifier: Identifier {
+                            name: String::from("a")
+                        },
+                        type_: Type::Atomic(Atomic::Integer(Integer::I32)),
+                        literal: Literal::Integer(2)
+                    }),
+                    ImplItem::Method(Function {
+                        attributes: Attributes { attributes: vec![] },
+                        asyncness: None,
+                        identifier: Identifier {
+                            name: String::from("b")
+                        },
+                        input: vec![],
+                        output: None
+                    })
+                ]
             }
         );
     }
