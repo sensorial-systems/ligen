f4c7c4cdbdabcc18281d3dd3308138b4d0166710
Danilo Guanabara <danilo@sensorial.systems>
2021-08-26 21:25:38 -0300

    Using ligen-c in the example

--- examples/counter/build.rs
+++ examples/counter/build.rs
@@ -1,16 +1,16 @@
 use ligen::prelude::*;
-// use ligen_c::CGenerator;
-// use ligen_cmake::{CMakeGenerator, Language};
-use ligen_csharp::CSharpGenerator;
+use ligen_c::CGenerator;
+use ligen_cmake::{CMakeGenerator, Language};
+// use ligen_csharp::CSharpGenerator;
 
 fn main() {
     if let Ok(project) = Project::read() {
-        // let c_generator = CGenerator::default();
-        // let cmake_generator = CMakeGenerator(Language::C);
-        let csharp_generator = CSharpGenerator::default();
-        // cmake_generator.generate(&project).expect("Failed to generate CMake project.");
-        // c_generator.generate(&project).expect("Failed to generate C bindings");
-        csharp_generator.generate(&project).expect("Failed to generate C# bindings.");
+        let c_generator = CGenerator::default();
+        let cmake_generator = CMakeGenerator(Language::C);
+        // let csharp_generator = CSharpGenerator::default();
+        cmake_generator.generate(&project).expect("Failed to generate CMake project.");
+        c_generator.generate(&project).expect("Failed to generate C bindings");
+        // csharp_generator.generate(&project).expect("Failed to generate C# bindings.");
     }
 }
 
