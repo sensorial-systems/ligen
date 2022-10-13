0d2cb3ea111d02dd8b39243d0eb1179afb87bee9
Danilo <danilo@sensorial.systems>
2022-10-13 16:27:06 -0300

    Using example dynamic library

--- examples/example-c/src/main.c
+++ examples/example-c/src/main.c
@@ -3,11 +3,10 @@
 //#include <example/functions/primitives.h>
 #include <assert.h>
 
-extern int example_add(int a, int b);
+extern __declspec(dllimport) int example_add(int a, int b);
 
 int main(int argc, char **argv) {
-    int a = 2;
-    int b = 3;
+    int a = 2, b = 3;
     printf("%d + %d = %d", a, b, example_add(a, b));
     return 0;
 }
