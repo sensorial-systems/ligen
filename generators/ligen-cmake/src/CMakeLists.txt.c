6419f6a016307a8c2caa5e07d8312e6cd0483bc0
Danilo Guanabara <danilo@sensorial.systems>
2022-10-17 23:57:15 -0300

    Moving dylib redist logic to the library CMakeLists

--- generators/ligen-cmake/src/CMakeLists.txt.c
+++ generators/ligen-cmake/src/CMakeLists.txt.c
@@ -11,10 +11,24 @@ ENDIF()
 ADD_LIBRARY(${{PROJECT_NAME}} INTERFACE)
 TARGET_INCLUDE_DIRECTORIES(${{PROJECT_NAME}} INTERFACE include)
 
+# Set ligen root directory where we can find the binary libraries.
+SET(LIGEN_DIR ${{CMAKE_CURRENT_SOURCE_DIR}}/../..)
+
 IF(UNIX)
-    TARGET_LINK_LIBRARIES(${{PROJECT_NAME}} INTERFACE "${{CMAKE_CURRENT_SOURCE_DIR}}/lib/lib${{PROJECT_NAME}}.a" -pthread -Wl,--no-as-needed -ldl -lm)
 ELSEIF(WIN32)
-    TARGET_LINK_LIBRARIES(${{PROJECT_NAME}} INTERFACE ${{CMAKE_CURRENT_SOURCE_DIR}}/../../rust/${{PROJECT_NAME}}/target/debug/ffi_${{PROJECT_NAME}}.dll.lib)
-    # TODO: Move files to ligen/libraries
-    # TARGET_LINK_LIBRARIES(${{PROJECT_NAME}} INTERFACE ${{CMAKE_CURRENT_SOURCE_DIR}}/../../libraries/${{PROJECT_NAME}}/debug/ffi_${{PROJECT_NAME}}.dll.lib)
+    SET(DYLIB_NAME ffi_${{PROJECT_NAME}}.dll)
+    SET(DYLIB_PATH ${{LIGEN_DIR}}/libraries/${{PROJECT_NAME}}/${{DYLIB_NAME}})
+    SET(STLIB_PATH ${{DYLIB_PATH}}.lib)
+    TARGET_LINK_LIBRARIES(${{PROJECT_NAME}} INTERFACE ${{STLIB_PATH}})
 ENDIF()
+
+# Detecting build type.
+SET(BUILD_TYPE $<IF:$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>>,Debug,Release>)
+
+# Setting dynamic library output path.
+SET(DYLIB_OUTPUT ${{CMAKE_BINARY_DIR}}/${{BUILD_TYPE}}/${{DYLIB_NAME}})
+
+# Dynamic library copy operation.
+ADD_CUSTOM_COMMAND(OUTPUT ${{DYLIB_OUTPUT}} COMMAND ${{CMAKE_COMMAND}} -E copy_if_different ${{DYLIB_PATH}} ${{DYLIB_OUTPUT}})
+ADD_CUSTOM_TARGET(${{PROJECT_NAME}}_DYLIB DEPENDS ${{DYLIB_OUTPUT}})
+ADD_DEPENDENCIES(${{PROJECT_NAME}} ${{PROJECT_NAME}}_DYLIB)
