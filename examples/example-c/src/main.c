f1e03c37469a815675c1bfc79f7899a3b64cc9a1
Danilo <danilo@sensorial.systems>
2022-10-20 15:38:19 -0300

    Fixing main.c example

--- examples/example-c/src/main.c
+++ examples/example-c/src/main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <assert.h>
 #include <example/functions/primitives.h>
-#include <rust.h>
 #include <ligen_rust_runtime.h>
 
+#define cassert(a) {\
+    assert(a);\
+    printf("%-64s... passed.\n", #a);\
+}
+
 int main(int argc, char **argv) {
-    assert(example_functions_primitives_add(1, 2) == 3);
-    assert(ligen_rust_runtime_test() == 123);
+    cassert(example_functions_primitives_add(1, 2) == 3);
+    cassert(ligen_rust_runtime_test() == 123);
     printf("All tests passed.\n");
     return 0;
 }
