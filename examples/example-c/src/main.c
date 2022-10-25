90573bf4075ae397052d05a8b7f5b79f7ff70ce9
Danilo Guanabara <danilo@sensorial.systems>
2022-10-24 22:25:47 -0300

    Library CMakeLists copy input dll if it's newer

--- examples/example-c/src/main.c
+++ examples/example-c/src/main.c
@@ -9,15 +9,17 @@
 }
 
 int main(int argc, char **argv) {
-//    cassert(example_functions_primitives_add_i8 (1, 2) == 3);
-//    cassert(example_functions_primitives_add_i16(1, 2) == 3);
-    cassert(example_functions_primitives_add_i32(1, 2) == 3);
-//    cassert(example_functions_primitives_add_i64(1, 2) == 3);
+    cassert(example_functions_primitives_add_i8 (1, -2) == -1);
+    cassert(example_functions_primitives_add_i16(1, -2) == -1);
+    cassert(example_functions_primitives_add_i32(1, -2) == -1);
+    cassert(example_functions_primitives_add_i64(1, -2) == -1);
+    cassert(example_functions_primitives_add_u8 (1,  2) ==  3);
+    cassert(example_functions_primitives_add_u16(1,  2) ==  3);
+    cassert(example_functions_primitives_add_u32(1,  2) ==  3);
+    cassert(example_functions_primitives_add_u64(1,  2) ==  3);
 
-//    cassert(example_functions_primitives_add_u8 (1, 2) == 3);
-//    cassert(example_functions_primitives_add_u16(1, 2) == 3);
-//    cassert(example_functions_primitives_add_u32(1, 2) == 3);
-//    cassert(example_functions_primitives_add_u64(1, 2) == 3);
+    cassert(example_functions_primitives_add_f32(1.0f, 2.0f) == 3.0f);
+    cassert(example_functions_primitives_add_f64(1.0 , 2.0 ) == 3.0);
 
     cassert(ligen_rust_runtime_test() == 123);
     printf("All tests passed.\n");
--- examples/example/src/functions/primitives.rs
+++ examples/example/src/functions/primitives.rs
@@ -1,1 +1,12 @@
+pub fn add_i8 (a:  i8, b:  i8) ->  i8 { a + b }
+pub fn add_i16(a: i16, b: i16) -> i16 { a + b }
 pub fn add_i32(a: i32, b: i32) -> i32 { a + b }
+pub fn add_i64(a: i64, b: i64) -> i64 { a + b }
+
+pub fn add_u8 (a:  u8, b:  u8) ->  u8 { a + b }
+pub fn add_u16(a: u16, b: u16) -> u16 { a + b }
+pub fn add_u32(a: u32, b: u32) -> u32 { a + b }
+pub fn add_u64(a: u64, b: u64) -> u64 { a + b }
+
+pub fn add_f32(a: f32, b: f32) -> f32 { a + b }
+pub fn add_f64(a: f64, b: f64) -> f64 { a + b }
