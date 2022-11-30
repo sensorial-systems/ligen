b69066b47261c1174167b6e7ffacdfc7066ccce3
Danilo Guanabara <danilo@sensorial.systems>
2022-11-30 00:41:37 -0300

    Removing deprecated function

--- ligen/ir/src/implementation/mod.rs
+++ ligen/ir/src/implementation/mod.rs
@@ -14,27 +14,3 @@ pub struct Implementation {
     /// Items field.
     pub items: Vec<ImplementationItem>,
 }
-
-impl Implementation {
-    /// Maps the dependencies in the method signatures.
-    pub fn dependencies(&self) -> Vec<Type> {
-        let mut deps: Vec<Type> = vec![];
-        for item in &self.items {
-            if let ImplementationItem::Method(method) = item {
-                method.inputs.clone().into_iter().for_each(|parameter| {
-                    if !deps.iter().any(|typ| typ == &parameter.type_) {
-                        deps.push(parameter.type_);
-                    }
-                });
-                if let Some(type_) = method.output.clone() {
-                    if !deps.iter().any(|typ| typ == &type_)
-                        && type_ != Type::from(Identifier::new("Self"))
-                    {
-                        deps.push(type_);
-                    }
-                }
-            }
-        }
-        deps
-    }
-}
