5a65438df15dc98de721ffb9eb8a50c47889d1f8
Danilo Guanabara <danilo@sensorial.systems>
2023-10-18 14:32:35 -0300

    Removing unecessary comment

--- ligen/symbols/src/interface.rs
+++ ligen/symbols/src/interface.rs
@@ -5,9 +5,7 @@ pub struct Interface {
     pub identifier: Identifier,
     pub constants: Vec<Identifier>,
     pub functions: Vec<Identifier>,
-    pub methods: Vec<Identifier>,
-    // TODO: Use this for inheritance, traits and alike:
-    // pub interfaces: Vec<Path>
+    pub methods: Vec<Identifier>
 }
 
 impl Interface {
