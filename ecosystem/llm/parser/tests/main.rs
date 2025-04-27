fdfc9ee74b845186dfae1389ac1646cb7155506d
Danilo Guanabara <danilo@magicblock.gg>
2025-04-27 18:57:30 -0300

    :white_check_mark: Disabling OpenAI API calls on tests because I don't want to pay for them :)

--- ecosystem/llm/parser/tests/main.rs
+++ ecosystem/llm/parser/tests/main.rs
@@ -4,12 +4,12 @@ use ligen_transformer::prelude::*;
 
 #[tokio::test]
 async fn main() -> Result<()> {
-    let parser = GeneralLlmParser::<Function>::new()?;
-
     if std::env::var("GITHUB_ACTIONS").is_ok() {
         println!("Running in GitHub Actions - skipping test because I don't want to pay for the API calls :)");
         return Ok(());
     }
+
+    let parser = GeneralLlmParser::<Function>::new()?;
 
     match parser.parse("pub async fn add(a: i32, b: i32) -> i32 { a + b }", &Config::default()).await {
         Ok(function) => println!("{:#?}", function),
