f39f32a4c750f7e2afcc4ba396154be4efa15cf1
Danilo Guanabara <danilo@magicblock.gg>
2025-05-12 01:51:36 -0300

    :sparkles: Generating Rust primitive types

--- ecosystem/rust/generator/src/identifier/mod.rs
+++ ecosystem/rust/generator/src/identifier/mod.rs
@@ -6,6 +6,16 @@ pub struct RustIdentifierGenerator;
 
 impl Generator<&Identifier, syn::Ident> for RustIdentifierGenerator {
     fn generate(&self, identifier: &Identifier, _config: &Config) -> Result<syn::Ident> {
-        Ok(syn::Ident::new(&identifier.to_string(), proc_macro2::Span::call_site()))
+        let name =
+        if identifier == &Identifier::u8() || identifier == &Identifier::u16() || identifier == &Identifier::u32() || identifier == &Identifier::u64() || identifier == &Identifier::u128()
+        || identifier == &Identifier::i8() || identifier == &Identifier::i16() || identifier == &Identifier::i32() || identifier == &Identifier::i64() || identifier == &Identifier::i128() 
+        || identifier == &Identifier::f32() || identifier == &Identifier::f64()
+        {
+            &identifier.to_string().to_lowercase()
+        } else {
+            &identifier.to_string()
+        };
+
+        Ok(syn::Ident::new(name, proc_macro2::Span::call_site()))
     }
 }
