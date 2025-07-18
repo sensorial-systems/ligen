e28068aaf14649043e738d3f3a9b19f1dc585dc0
Danilo Guanabara <danilo@magicblock.gg>
2025-07-18 20:10:09 -0300

    :recycle: Passing LLM tests if API KEY is not set

--- ecosystem/llm/parser/tests/main.rs
+++ ecosystem/llm/parser/tests/main.rs
@@ -4,39 +4,36 @@ use ligen_transformer::{assert::async_assert_eq, prelude::*};
 
 #[tokio::test]
 async fn function() -> Result<()> {
-    if std::env::var("GITHUB_ACTIONS").is_ok() {
-        println!("Running in GitHub Actions - skipping test because I don't want to pay for the API calls :)");
-        return Ok(());
+    if let Ok(parser) = GeneralLlmParser::<Function>::new() {
+        async_assert_eq(parser, function::mock::function_input_output(), "pub fn test(a: i32, b: i32) -> i32 { a + b }".to_string()).await?;
+    } else {
+        println!("Skipping test because I don't want to pay for the API calls :)");
     }
 
-    async_assert_eq(GeneralLlmParser::<Function>::new()?, function::mock::function_input_output(), "pub fn test(a: i32, b: i32) -> i32 { a + b }".to_string()).await?;
     Ok(())
 }
 
 #[tokio::test]
 async fn module() -> Result<()> {
-    if std::env::var("GITHUB_ACTIONS").is_ok() {
-        println!("Running in GitHub Actions - skipping test because I don't want to pay for the API calls :)");
-        return Ok(());
+    if let Ok(parser) = GeneralLlmParser::<Module>::new() {
+        async_assert_eq(parser, module::mock::module_types(), "pub mod types { pub struct Structure; pub enum Enumeration; }".to_string()).await?;
+    } else {
+        println!("Skipping test because I don't want to pay for the API calls :)");
     }
 
-    async_assert_eq(GeneralLlmParser::<Module>::new()?, module::mock::module_types(), "pub mod types { pub struct Structure; pub enum Enumeration; }".to_string()).await?;
     Ok(())
 }
 
 #[tokio::test]
 async fn project_discovery() -> Result<()> {
-    if std::env::var("GITHUB_ACTIONS").is_ok() {
-        println!("Running in GitHub Actions - skipping test because I don't want to pay for the API calls :)");
-        return Ok(());
+    if let Ok(parser) = GeneralLlmParser::<ProjectDiscovery>::new() {
+        let project_directory = ProjectFiles::from_path("/home/notdanilo/dev/3rd/NotaFiscalSP")?; // FIXME: This is a hardcoded path.
+        let project_discovery = parser.parse(&project_directory.to_string(), &Config::default()).await?;
+        let structured_project_files = StructuredProjectFiles::new(&project_directory, &project_discovery);
+        println!("{structured_project_files:#?}");
+    } else {
+        println!("Skipping test because I don't want to pay for the API calls :)");
     }
 
-    let project_directory = ProjectFiles::from_path("/home/notdanilo/dev/3rd/NotaFiscalSP")?; // FIXME: This is a hardcoded path.
-    println!("{project_directory}");
-    let project_discovery = GeneralLlmParser::<ProjectDiscovery>::new()?;
-    let project_discovery = project_discovery.parse(&project_directory.to_string(), &Config::default()).await?;
-    println!("{project_discovery:#?}");
-    let structured_project_files = StructuredProjectFiles::new(&project_directory, &project_discovery);
-    println!("{structured_project_files:#?}");
     Ok(())
 }
