3f17024283e81c9e100048718b6cf57a4ccd5a1c
Danilo Guanabara <danilo@sensorial.systems>
2026-02-13 16:37:27 -0300

    :recycle: Fixing test

--- ligen/idl/src/object/mock.rs
+++ ligen/idl/src/object/mock.rs
@@ -2,9 +2,10 @@ pub use crate::*;
 
 pub fn constant() -> Object {
     Object {
+        visibility: Visibility::Private,
         mutability: Mutability::Constant,
         identifier: "CONSTANT".into(),
         type_: Type::boolean(),
-        literal: false.into()
+        literal: false.into(),
     }
 }
